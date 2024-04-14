c77f006a5c4828a95eaf4e86605c379734a1da0e
Pankhur <pankhurtulsyan@gmail.com>
2024-04-14 07:08:13 +0000

    Solved exercise 1

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,7 +12,11 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
+
+//Create a buffer channel
+var sem = make(chan struct{}, 1)
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
@@ -23,7 +27,12 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 		return
 	}
 
+	//Implement semaphore concept
+	sem <- struct{}{}
 	body, urls, err := fetcher.Fetch(url)
+	//Since it's mentioned one page per second so we have to introduce second as well
+	time.Sleep(time.Second)
+	<-sem
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -32,18 +41,24 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	fmt.Printf("found: %s %q\n", url, body)
 
 	wg.Add(len(urls))
+	
 	for _, u := range urls {
+		
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
 		go Crawl(u, depth-1, wg)
+		
 	}
 	return
 }
 
 func main() {
 	var wg sync.WaitGroup
-
+	//sem := make(chan struct{})
+	
 	wg.Add(1)
+	//sem <- struct{}{}
 	Crawl("http://golang.org/", 4, &wg)
+	//<-sem
 	wg.Wait()
 }
